f0c8cd40556da3c26689c66c47407bc2030e698d
unknown <lefebvre.joachim7@gmail.com>
2018-08-09 15:30:36 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,70 @@
+//              EXO 1
+
+// var prenom = prompt("Quel est ton prénom ?");
+
+// alert("Bonjour " + prenom + " !");
+
+//              EXO 2
+
+// var red = document.getElementById("red");
+// var blue = document.getElementById("blue");
+// var green = document.getElementById("green");
+
+// red.style.border = "1px solid red";
+// blue.style.border = "1px solid blue";
+// green.style.border = "1px solid green";
+
+//              EXO 3
+
+// var pseudo = prompt("Saisissez un pseudo");
+// var mdp = prompt("Saisissez un mdp");
+
+// var entrepseudo = prompt("Saisissez votre pseudo enregistré");
+// var entremdp = prompt("Saisissez votre mdp enregistré");
+
+// if (pseudo === entrepseudo && mdp === entremdp) {
+//     alert("Connexion réussie !");
+// }
+// else {
+//     alert("Erreur, veuillez recommencer.");
+// }
+
+//              EXO 4
+
+// var nombre = prompt("Saisissez un nombre entre 1 et 4");
+
+// if (nombre == 1) {
+    
+// }
+// else if (nombre == 2) {
+
+// }
+// else if (nombre == 3) {
+
+// }
+// else if (nombre == 4) {
+
+// }
+// else {
+    
+// }
+
+//              TEST
+
+// function myLocalScope() {
+//     'use strict'; // you shouldn't need to edit this line
+    
+//     console.log(myVar);
+//   }
+//   myLocalScope(myVar);
+  
+//   // Run and check the console
+//   // myVar is not defined outside of myLocalScope
+//   console.log(myVar);
+
+//   var meteo = prompt("Quel temps fait-il ?");
+//   var neurones = 10000000000000000;
+
+//   if (meteo = "canicule") {
+//       neurones = 0;
+//   }
